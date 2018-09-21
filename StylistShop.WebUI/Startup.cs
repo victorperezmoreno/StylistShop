10fd87e8a9071caefba03e828fff378ba8056907
Victor Perez Moreno <40905432+victorperezmoreno@users.noreply.github.com>
2018-09-21 18:34:31 -0400

    Initial solution setup
    All projects/folders created and ready to start coding

---
+++ StylistShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StylistShop.WebUI.Startup))]
+namespace StylistShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
