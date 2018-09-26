08585a674b69599c606d19d3964154d5ee4c7afd
Victor Perez Moreno <40905432+victorperezmoreno@users.noreply.github.com>
2018-09-25 23:35:56 -0400

    Product List added to Home
    List of products displayed at Home controller, lso added Details Controller/View

--- StylistShop.WebUI/Controllers/HomeController.cs
+++ StylistShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using StylistShop.Core.Contracts;
+using StylistShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,36 @@ namespace StylistShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        //Interface implementation, so we combine generics with interface for Product and category
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+
+        //Create a constructor to initialize repository
+        //Passing a class that implements an IRepository product and an IRepository productCategory
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            //Send a list of products to the View
+            var products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Details(string Id)
+        {
+            var product = context.Find(Id);
+            if (product != null)
+            {
+                return View(product);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
         }
 
         public ActionResult About()
