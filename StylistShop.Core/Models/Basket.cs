6a50f5dafd4c65699b6f09e21f4fd4d42ed6182a
Victor Perez Moreno <40905432+victorperezmoreno@users.noreply.github.com>
2018-09-27 17:29:44 -0400

    Basket Model Created
    Added Basket and BasketItems classes to the model, also added classes to the datacontext. Performed model migration and updated DB

---
+++ StylistShop.Core/Models/Basket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StylistShop.Core.Models
+{
+    public class Basket : BaseEntity
+    {
+        //virtual indicates that is is a lazy loading. Load all the items in basket
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+        //Constructor
+        public Basket()
+            {
+            this.BasketItems = new List<BasketItem>();
+            }
+
+    }
+}
--- StylistShop.DataAccess.SQL/DataContext.cs
+++ StylistShop.DataAccess.SQL/DataContext.cs
@@ -19,6 +19,8 @@ namespace StylistShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
 
     }
 }
